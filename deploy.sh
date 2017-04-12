81d7da53ba454aa3430e8629aa556251acf6df21
Andrey Kuzmin <unsoundscapes@gmail.com>
2017-04-12 21:57:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,10 +10,8 @@ cp -r snd dest/
 # compile JS using Elm
 elm make src/Main.elm --yes --output dest/index.html
 
-cd dest
-
 # replace ../snd/theme.ogg with snd/theme.ogg
-sed 's/\.\.\/snd\/theme.ogg/snd\/theme.ogg/g' index.html > index.html.tmp && mv index.html.tmp index.html
+sed 's/\.\.\/snd\/theme.ogg/snd\/theme.ogg/g' dest/index.html > dest/index.html.tmp && mv dest/index.html.tmp dest/index.html
 
 # publish to itch.io
 butler push dest unsoundscapes/mogee:html
