a3ac9b32a9ce02739ee2e3357e06d171d3e68f16
Andrey Kuzmin <andrey.kuzmin@soundcloud.com>
2017-04-19 21:57:43 +0200

    Steal the focus

--- deploy.sh
+++ deploy.sh
@@ -15,5 +15,12 @@ cd dest
 # replace ../snd/theme.ogg with snd/theme.ogg
 sed 's/\.\.\/snd\/theme.ogg/snd\/theme.ogg/g' index.html > index.html.tmp && mv index.html.tmp index.html
 
+# steal focus
+cat >> index.html <<EOT
+<script>
+   window.focus();
+</script>
+EOT
+
 # publish to itch.io
-butler push dest unsoundscapes/mogee:html
+butler push ../dest unsoundscapes/mogee:html
